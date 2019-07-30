2df4cfa2230ad3f4b2f0d3d551bf4ed1aea07a22
GalvinGao <GalvinGao@users.noreply.github.com>
2019-07-30 16:49:47 +0800

    feat(getters): sorts item and calculates extra statistics

--- src/utils/getters.js
+++ src/utils/getters.js
@@ -4,18 +4,45 @@ let Getters = {};
 
 Getters.item = {
   byItemId (itemId) {
-    if (itemId === "furni") return {
-      "itemId": "furni",
-      "name": "家具",
-      "sortId": 9999,
-      "rarity": 0,
-      "itemType": "FURN",
-      "spriteCoord": [0, 0]
-    };
-    return store.state.data.items.find(el => {
+    // if (itemId === "furni") return {
+    //   "itemId": "furni",
+    //   "name": "家具",
+    //   "sortId": 9999,
+    //   "rarity": 0,
+    //   "itemType": "FURN",
+    //   "spriteCoord": [0, 0]
+    // };
+    return this.all(false).find(el => {
       return el.itemId === itemId
     })
   },
+  all (sort=true) {
+    if (!store.state.data.items) return [];
+    let r = store.state.data.items;
+    r.forEach(el => {
+      const meta = {
+        CARD_EXP: {
+          name: "作战记录",
+          icon: "mdi-card-bulleted",
+          color: "light-blue"
+        },
+        MATERIAL: {
+          name: "材料",
+          icon: "mdi-cube-outline",
+          color: "lime"
+        },
+        FURN: {
+          name: "家具",
+          icon: "mdi-lamp",
+          color: "blue-grey"
+        }
+      };
+      
+      el.meta = meta[el.itemType]
+    });
+    if (sort) r.sort((a, b) => a.sortId - b.sortId)
+    return r
+  }
 }
 Getters.limitations = {
   byStageId (stageId) {
@@ -26,9 +53,21 @@ Getters.limitations = {
 }
 Getters.statistics = {
   byItemId (itemId) {
-    return store.state.data.resultMatrix.matrix.filter(el => {
+    let result = store.state.data.resultMatrix.matrix.filter(el => {
       return el.itemId === itemId
-    })
+    });
+
+    result.forEach(el => {
+      let stage = Getters.stages.byStageId(el.stageId);
+
+      el.stage = stage;
+
+      el.percentage = (el.quantity / el.times);
+      el.percentageText = `${(el.percentage * 100).toFixed(2)}%`;
+
+      el.apPPR = (stage.apCost / el.percentage).toFixed(2)
+    });
+    return result
   },
   byStageId (stageId) {
     let result = store.state.data.resultMatrix.matrix.filter(el => {
