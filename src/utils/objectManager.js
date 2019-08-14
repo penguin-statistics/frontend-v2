1bd6358f2bd3246e08967940eaf937b7a350f393
GalvinGao <GalvinGao@users.noreply.github.com>
2019-08-15 01:11:37 +0800

    refactor(objectmanager): combined transformers into one function to ensure the clearity of the api

--- src/utils/objectManager.js
+++ src/utils/objectManager.js
@@ -1,19 +1,17 @@
-/* Object Life-cycle manager
- *
+/*
+ * Object Life-cycle manager
  * Automatically fetch data when passed object's TTL and provide getter api
  */
 
 import service from './service'
-import store from '@/store'
 
 class ObjectManager {
-    constructor({id, transforms, ttl, retry, ajaxHooks}) {
+    constructor({id, transform, ttl, ajaxHooks}) {
         this.id = id; // object id, will be used to determine the api path
-        this.apiPath = `/api/${id}`; // the api path to GET
+        this.apiPath = `/${id}`; // the api path to GET
 
-        this.transforms = transforms; // [{ key: function }] | key: object path to transform; function: (value) => {return transformedData}
+        this.transform = transform; // function | function: data => {return transformedData}
         this.ttl = ttl; // time-to-live (TTL), in milliseconds
-        this.retry = retry; // { retries: the total amount of retries, factor: multiplier between retries }
         this.ajaxHooks = ajaxHooks; // { request (): fired when request begins, response (Promise): fired when received response }
 
         this.cache = {
@@ -23,44 +21,28 @@ class ObjectManager {
         }
     }
 
-    _isOutdated () {
-        return this.cache.updatedAt + this.ttl < Date.now()
-    }
-
-    // _transform transforms argument `data` and return the transformed dataset
-    _transform (data) {
-        let context = this;
-        // for each data object in the data array we've got
-        data.forEach(obj => {
-            // for each [key, value] pairs in the data object
-            for (let key in obj) {
-                if (!(key in context.transforms)) continue; // if there's no transform function for this field then skip it
-                if (!obj.hasOwnProperty(key)) continue; // check for ownProperty
-                obj[key] = context.transforms[key](obj[key]) // otherwise apply the corresponding function to the value
-            }
-        });
-        return data
+    _isCacheValid () {
+        console.debug("cache valid:", this.cache.updatedAt + this.ttl > Date.now(), "|", this.cache.updatedAt, this.ttl, Date.now());
+        return this.cache.updatedAt + this.ttl > Date.now()
     }
 
     // _get returns local cache if ttl has been fulfilled, and fetches external api when
     // the ttl of local cache has outdated or the local cache is empty / not available
     async _get () {
         let context = this;
-        if (context._isOutdated()) {
+        if (context._isCacheValid()) {
             // valid cache
+            console.log("valid");
             return Promise.resolve(context.cache.data)
         } else {
             // outdated cache, fetch api
-            // TODO: add retry
+            console.log("outdated");
             context.ajaxHooks.request();
             let response = service.get(context.apiPath)
               .then(({data}) => {
-                  context.cache.data = context._transform(data);
+                  context.cache.data = context.transform(data);
                   context.cache.updatedAt = Date.now();
                   return context.cache.data
-              })
-              .catch(({err}) => {
-                  store.dispatch('ajaxFailed', err.errorMessage)
               });
             context.ajaxHooks.response(response);
             return response
@@ -72,13 +54,14 @@ class ObjectManager {
     // getOne returns a object matches the condition
     async getOne (key, value) {
         let data = await this._get();
-        return data.find(v => v[key] === value)
+        console.log(data);
+        return await data.find(v => v[key] === value)
     }
 
     // getAll returns an array that contains all the items which is matching the condition
     async getAll (key, value) {
         let data = await this._get();
-        return data.filter(v => v[key] === value)
+        return await data.filter(v => v[key] === value)
     }
 }
 
