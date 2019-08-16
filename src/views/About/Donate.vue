68ee0549652661d3a7f3c7daf62bbc51cbd260db
Asahi Zhang <asahi@alpgo.cc>
2019-08-16 23:54:12 +0800

    feat(home): migrate old static pages to the new site
    Refine Home/Changelog/Contribute/Donate/Links/Members page
    
    fix #15

--- src/views/About/Donate.vue
+++ src/views/About/Donate.vue
@@ -1,11 +1,81 @@
 <template>
-  <v-layout>
-    Donate
-  </v-layout>
+  <v-card class="mx-auto bkop-light">
+    <v-card-title>
+      <div><del>用户量激增，服务器月费上涨，企鹅数据的运营陷入了危机。站长红白为了保护喜爱的企鹅数据而决定...</del></div>
+    </v-card-title>
+
+    <v-card-title>
+      <div>
+        随着访问量的增加，目前的小水管服务器已经有些不堪重负了。如果各位刀客塔能捐助一些
+        <v-tooltip
+          top
+        >
+          <template v-slot:activator="{ on }">
+            <span
+              style="border-bottom:1px dashed #ccc;"
+              v-on="on"
+            >服务器费用</span>
+          </template>
+          pocky、阿噗噜派也可
+        </v-tooltip>
+        ，我们将不胜感激!
+      </div>
+    </v-card-title>
+    <v-layout
+      row
+      wrap
+    >
+      <v-flex
+        xs6
+        md6
+      >
+        <v-card class="transparent">
+          <v-img
+            src="https://penguin-stats.s3-ap-southeast-1.amazonaws.com/alipay.png"
+            alt="支付宝二维码"
+          />
+
+          <v-card-title primary-title>
+            <v-layout
+              align-center
+              justify-center
+              row
+              fill-height
+            >
+              支付宝二维码
+            </v-layout>
+          </v-card-title>
+        </v-card>
+      </v-flex>
+
+      <v-flex
+        xs6
+        md6
+      >
+        <v-card class="transparent">
+          <v-img
+            src="https://penguin-stats.s3-ap-southeast-1.amazonaws.com/wechat.png"
+            alt="微信支付二维码"
+          />
+
+          <v-card-title primary-title>
+            <v-layout
+              align-center
+              justify-center
+              row
+              fill-height
+            >
+              微信支付二维码
+            </v-layout>
+          </v-card-title>
+        </v-card>
+      </v-flex>
+    </v-layout>
+  </v-card>
 </template>
 
 <script>
-  export default {
-    name: 'Donate'
-  }
+export default {
+  name: "Donate"
+};
 </script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,62 @@
 <template>
-  <v-layout>
-    <v-flex />
-  </v-layout>
+  <v-container
+    fluid
+    grid-list-sm
+  >
+    <v-layout
+      row
+      wrap
+    >
+      <v-flex
+        xs12
+        sm6
+      >
+        <Contact />
+      </v-flex>
+      <v-flex
+        xs12
+        sm6
+      >
+        <Contribute />
+      </v-flex>
+      <v-flex
+        xs12
+        sm6
+      >
+        <Donate />
+      </v-flex>
+      <v-flex
+        sm12
+        md6
+      >
+        <Links />
+      </v-flex>
+      <v-flex
+        xs12
+        md12
+      >
+        <Members />
+      </v-flex>
+      <v-flex
+        xs12
+        md12
+      >
+        <Changelog />
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
 
 <script>
-  export default {
-    name: 'Home'
-  }
+import Changelog from "@/views/About/Changelog";
+import Members from "@/views/About/Members";
+import Contact from "@/views/About/Contact";
+import Contribute from "@/views/About/Contribute";
+import Donate from "@/views/About/Donate";
+import Links from "@/views/About/Links";
+export default {
+  name: "Home",
+  components: { Changelog, Contact, Contribute, Members, Donate, Links },
+  data: () => ({})
+};
 </script>
