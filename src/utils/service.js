e0c920671374f806efa64cdcb499f9436115347d
GalvinGao <galvingaoper@gmail.com>
2020-01-06 13:43:01 -0500

    build(service): fix unable to get api data under now build

--- src/utils/service.js
+++ src/utils/service.js
@@ -3,8 +3,9 @@ import Console from "@/utils/Console";
 
 const service = axios.create({
   // on non-production environments the client will try to send any unknown requests (requests that did not match a static file)
-  // to https://penguin-stats.io/PenguinStats/api via `webpack-dev-server`, described in vue.config.js
-  baseURL: "/PenguinStats/api"
+  // to https://penguin-stats.io/PenguinStats/api via `webpack-dev-server`, described in `vue.config.js`
+  // in order to use local server, please change the corresponding setting in `vue.config.js`
+  baseURL: process.env.NODE_ENV === "development" ? "/PenguinStats/api" : "https://penguin-stats.io/PenguinStats/api"
 });
 
 // Add a response interceptor
