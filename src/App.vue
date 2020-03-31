250435000f3f7a8b76aea77ab4f5701c3e6ed1b5
AlvISsReimu <alvissreimu@gmail.com>
2020-04-01 03:22:23 +0800

    feat(icon): change icon to reunion version

--- src/App.vue
+++ src/App.vue
@@ -491,10 +491,13 @@ export default {
       function imageUrl (character) {
         return `${config.cdn.global}/logos/penguin_stats_logo_${character}.png`
       }
-      this.randomizedLogo = random < .25 ? imageUrl("exia")
-        : random < .5 ? imageUrl("texas")
-          : random < .75 ? imageUrl("sora")
-            : imageUrl("croissant")
+      this.randomizedLogo = random < (1.0/7) ? imageUrl("talulah")
+        : random < (2.0/7) ? imageUrl("w")
+          : random < (3.0/7) ? imageUrl("skullshatterer")
+          : random < (4.0/7) ? imageUrl("frostnova")
+          : random < (5.0/7) ? imageUrl("crownslayer")
+          : random < (6.0/7) ? imageUrl("faustus")
+            : imageUrl("mephisto")
     },
     changeLocale (localeId, save=true) {
       dayjs.locale(localeId)
