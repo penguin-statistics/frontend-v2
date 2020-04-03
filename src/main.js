12b8c57259eb184e11c8aac78e30f0a6fdd0bd26
GalvinGao <galvingaoper@gmail.com>
2020-04-02 21:39:00 -0400

    improvement(sentry): ignore some errors that is not our fault :(

--- src/main.js
+++ src/main.js
@@ -26,6 +26,35 @@ if (production) {
     ],
     tracesSampleRate: 0.05,
     release: 'frontend-v2@' + (config.version || 'unknown'),
+    ignoreErrors: [
+      //// START: those errors are found at https://docs.sentry.io/platforms/javascript/#decluttering-sentry
+      'top.GLOBALS',
+      // See: http://blog.errorception.com/2012/03/tale-of-unfindable-js-error.html
+      'originalCreateNotification',
+      'canvas.contentDocument',
+      'MyApp_RemoveAllHighlights',
+      'http://tt.epicplay.com',
+      'Can\'t find variable: ZiteReader',
+      'jigsaw is not defined',
+      'ComboSearch is not defined',
+      'http://loading.retry.widdit.com/',
+      'atomicFindClose',
+      // Facebook borked
+      'fb_xd_fragment',
+      // ISP "optimizing" proxy - `Cache-Control: no-transform` seems to
+      // reduce this. (thanks @acdha)
+      // See http://stackoverflow.com/questions/4113268
+      'bmi_SafeAddOnload',
+      'EBCallBackMessageReceived',
+      // See http://toolbar.conduit.com/Developer/HtmlAndGadget/Methods/JSInjection.aspx
+      'conduitPage',
+      //// END
+
+      //// Those are our customized ones
+      "QuotaExceededError",
+      "vivoNewsDetailPage",
+      "Request aborted"
+    ]
   });
 }
 
