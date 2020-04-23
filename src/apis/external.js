1ef6b9fad08aab62243e6b6a163e9eba0ec8bdcd
GalvinGao <galvingaoper@gmail.com>
2020-04-22 23:48:55 -0400

    fix(ajax): fix ajax failed for ipapi

--- src/apis/external.js
+++ src/apis/external.js
@@ -2,6 +2,8 @@ import service from '@/utils/service'
 
 export default {
   geoip () {
-    return service.get("https://ipapi.co/json/")
+    return service.get("https://ipapi.co/json/", {
+      withCredentials: false
+    })
   }
 }
