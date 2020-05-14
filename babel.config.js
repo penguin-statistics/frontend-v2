30a9a3898215c05b2f542beb7ff4e94e7c96d281
GalvinGao <galvingaoper@gmail.com>
2020-05-14 10:52:36 -0400

    fix(babel): fix d3.js undefined document

--- babel.config.js
+++ babel.config.js
@@ -3,8 +3,8 @@ module.exports = {
     '@vue/app'
   ],
   plugins: [
-    ["@babel/plugin-transform-modules-commonjs", {
-      loose: true
-    }]
+    // ["@babel/plugin-transform-modules-commonjs", {
+    //   loose: true
+    // }]
   ]
 }
