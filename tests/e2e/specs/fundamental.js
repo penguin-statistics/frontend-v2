ac85e0a113ee2fe0fcb076df0596dae1e0ce1161
GalvinGao <galvingaoper@gmail.com>
2020-07-09 21:31:14 +0800

    test(removal): temporarily removed testing files due to blocking merge
    this change shall be splitted into a new branch instead of cluttering into this hotfix branch.
    testing WILL be added later to ensure proper UI implementation and basic application functions.

--- tests/e2e/specs/fundamental.js
+++
@@ -1,8 +0,0 @@
-// https://docs.cypress.io/api/introduction/api.html
-
-describe('basic tests', () => {
-  it('loads the app successfully', () => {
-    cy.visit('/')
-  })
-
-})
--- tests/e2e/specs/screenshot.js
+++
@@ -1,8 +0,0 @@
-// https://docs.cypress.io/api/introduction/api.html
-
-describe('preview screenshots', () => {
-  it('should take screenshot of homepage', () => {
-    cy.visit('/')
-    cy.screenshot('homepage')
-  })
-})
