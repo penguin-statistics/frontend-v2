e980da8c3f01138c3f084d1b59532ac97b5efc33
GalvinGao <galvingaoper@gmail.com>
2020-07-23 13:12:20 +0800

    improvement(forgotaccount): properly set user cookie when using forgotaccount login

--- src/components/toolbar/ForgotAccount.vue
+++ src/components/toolbar/ForgotAccount.vue
@@ -1,3 +1,32 @@
+<i18n>
+  {
+    "en": {
+      "failed": {
+        "message": "Failed to log in: {message}",
+        "notfound": "This User ID cannot be found. Please not that this is not the ID in the game. You will get one after your first drop report."
+      }
+    },
+    "ja": {
+      "failed": {
+        "message": "ログイン失敗：{message}",
+        "notfound": "指定されたユーザーIDが見つかりませんでした。初めてドロップをアップロードすると、ユーザーIDを自動的に取得できます。"
+      }
+    },
+    "ko": {
+      "failed": {
+        "message": "로그인에 실패했습니다: {message}",
+        "notfound": "지정된 사용자 ID를 찾을 수 없습니다"
+      }
+    },
+    "zh": {
+      "failed": {
+        "message": "登录失败：{message}",
+        "notfound": "未找到此用户ID。请注意这不是游戏内的ID。在本站汇报一次掉落数据即可自动获得。"
+      }
+    }
+  }
+</i18n>
+
 <template>
   <v-card>
     <v-card-title class="title pb-1">
@@ -46,6 +75,8 @@
                 icon
                 color="primary"
                 class="mr-1"
+                :loading="loading === userId.id"
+                :disabled="loading && loading !== userId.id"
                 @click="loginAsUserId(userId.id)"
               >
                 <v-icon>
@@ -55,6 +86,7 @@
               <v-btn
                 icon
                 color="error"
+                :disabled="loading === userId.id"
                 @click="deleteUserId(userId.id)"
               >
                 <v-icon>
@@ -78,9 +110,16 @@
   import Subheader from "@/components/global/Subheader";
   import {mapGetters} from "vuex";
   import timeFormatter from "@/utils/timeFormatter";
+  import service from "@/utils/service";
+  import Console from "@/utils/Console";
   export default {
     name: "ForgotAccount",
     components: {Subheader},
+    data() {
+      return {
+        loading: false
+      }
+    },
     computed: {
       ...mapGetters("options", ["userIdHistory"]),
       userIds() {
@@ -103,8 +142,23 @@
         this.$store.commit("options/removeUserIdHistory", userId)
       },
       loginAsUserId(userId) {
-        this.$emit('loggedIn')
-        this.$store.dispatch("auth/login", userId)
+        this.loading = userId
+        service.post("/users", userId, {headers: {'Content-Type': 'text/plain'}})
+          .then(() => {
+            this.$emit('loggedIn')
+            this.$store.dispatch("auth/login", userId)
+          })
+          .catch((err) => {
+            Console.info("AccountManager", "auth failed", err)
+            if (err.response && err.response.status && err.response.status === 404) {
+              this.error = this.$t('failed.message', {message: this.$t('failed.notfound')})
+            } else {
+              this.error = this.$t('failed.message', {message: err.errorMessage})
+            }
+          })
+          .finally(() => {
+            this.loading = false
+          })
       },
       up () {
         this.$forceUpdate()
