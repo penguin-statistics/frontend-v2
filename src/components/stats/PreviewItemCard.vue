d8c4a3a878ae8f8883ef475bb59373ffc291ba71
GalvinGao <galvingaoper@gmail.com>
2020-07-29 23:59:22 +0800

    improvement(previewcard): fix layout issue when displaying on small device

--- src/components/stats/PreviewItemCard.vue
+++ src/components/stats/PreviewItemCard.vue
@@ -3,7 +3,7 @@
     color="background"
     elevation="9"
   >
-    <v-card-title :class="{'pb-0': stats.data.length}">
+    <v-card-title>
       <ItemIcon
         :item="item"
         :ratio="0.5"
@@ -14,72 +14,70 @@
     </v-card-title>
     <v-card-text v-if="stats.data.length">
       <v-simple-table dense>
-        <table>
-          <thead>
-            <tr>
-              <th>
-                {{ $t('stats.headers.stage') }}
-              </th>
-              <th class="font-weight-bold">
-                {{ $t('stats.headers.percentage') }}
-                <v-icon small>
-                  mdi-sort-descending
-                </v-icon>
-              </th>
-              <th>
-                {{ $t('stats.headers.quantity') }}
-                /
-                {{ $t('stats.headers.times') }}
-              </th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr
-              v-for="stat in stats.data"
-              :key="stat.stageId"
-              class="monospace"
-              :class="{'orange--text font-weight-bold': highlight === stat.stageId}"
-            >
-              <td class="d-flex align-center">
-                <v-icon
-                  v-if="highlight === stat.stageId"
-                  color="orange"
-                  small
-                  class="mr-1"
-                >
-                  mdi-arrow-right-circle
-                </v-icon>
-                <v-icon
-                  :color="highlight === stat.stageId ? 'orange' : ''"
-                  small
-                  class="mr-1"
-                >
-                  {{ stat.zone.icon }}
-                </v-icon>
-                {{ stat.stageCode }}
-              </td>
-              <td>
-                {{ stat.percentageText }}
-              </td>
-              <td>
-                {{ stat.quantity }} / {{ stat.times }}
-              </td>
-            </tr>
-            <tr
-              v-if="stats.more"
-              style="background: inherit !important;"
-            >
-              <td
-                colspan="2"
-                class="text-center"
+        <thead>
+          <tr>
+            <th>
+              {{ $t('stats.headers.stage') }}
+            </th>
+            <th class="font-weight-bold">
+              {{ $t('stats.headers.percentage') }}
+              <v-icon small>
+                mdi-sort-descending
+              </v-icon>
+            </th>
+            <th>
+              {{ $t('stats.headers.quantity') }}
+              /
+              {{ $t('stats.headers.times') }}
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr
+            v-for="stat in stats.data"
+            :key="stat.stageId"
+            class="monospace"
+            :class="{'orange--text font-weight-bold': highlight === stat.stageId}"
+          >
+            <td>
+              <v-icon
+                v-if="highlight === stat.stageId"
+                color="orange"
+                small
+                class="mr-1"
               >
-                <v-icon color="grey">
-                  mdi-dots-horizontal
-                </v-icon>
-              </td>
-            </tr>
-          </tbody>
-        </table>
+                mdi-arrow-right-circle
+              </v-icon>
+              <v-icon
+                :color="highlight === stat.stageId ? 'orange' : ''"
+                small
+                class="mr-1"
+              >
+                {{ stat.zone.icon }}
+              </v-icon>
+              {{ stat.stageCode }}
+            </td>
+            <td>
+              {{ stat.percentageText }}
+            </td>
+            <td>
+              {{ stat.quantity }} / {{ stat.times }}
+            </td>
+          </tr>
+          <tr
+            v-if="stats.more"
+            style="background: inherit !important;"
+          >
+            <td
+              colspan="3"
+              class="text-center"
+            >
+              <v-icon color="grey">
+                mdi-dots-horizontal
+              </v-icon>
+            </td>
+          </tr>
+        </tbody>
       </v-simple-table>
     </v-card-text>
   </v-card>
@@ -118,16 +116,16 @@
           .filter(el => existUtils.existence(el, true))
 
           .sort((a, b) => b.percentage - a.percentage)
-          .slice(0, pagination)
-          .map(el => {
-            return {
-              ...el,
-              stageCode: strings.translate(el.stage, "code")
-            }
-          })
 
         return {
-          data,
+          data: data
+            .slice(0, pagination)
+            .map(el => {
+              return {
+                ...el,
+                stageCode: strings.translate(el.stage, "code")
+              }
+            }),
           more: data.length > pagination
         }
       },
