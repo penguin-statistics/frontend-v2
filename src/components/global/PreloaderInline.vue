9a998ed082df9201a3f385d1e61a08fb4e103fc5
GalvinGao <galvingaoper@gmail.com>
2020-09-17 00:46:59 +0800

    refactor(preloader): rewrite preloader mechanism to avoid using gif
    mobile safari does not support transparent gif - or at least it behaves in an unexpected way which -
    it displays white background on some of the frames instead of transparent background (such gif works
    well on chrome). in order to avoid poor UX we have migrated the solution to be using a PNG sequence
    in order to compensate such lack of feature that mobile safari has.

--- src/components/global/PreloaderInline.vue
+++ src/components/global/PreloaderInline.vue
@@ -1,27 +1,41 @@
 <template>
-  <v-img
-    :src="currentSrc"
-    :aspect-ratio="1"
-    :height="size.image"
-    :width="size.image"
-    contain
-    class="mx-auto"
+  <div
+    :style="size.image"
+    class="animated-preloader-wrapper"
   >
-    <template v-slot:placeholder>
-      <div class="d-flex fill-height align-center justify-center">
+    <transition
+      mode="out-in"
+      appear
+      duration="125"
+      name="fade-transition"
+    >
+      <div
+        v-if="ready"
+        key="preloader"
+        class="animated-preloader mx-auto"
+
+        :style="{...size.image, backgroundImage: `url(${url})`}"
+      />
+      <div
+        v-else
+        key="circular"
+        class="d-flex fill-height align-center justify-center"
+      >
         <v-progress-circular
           indeterminate
           :size="size.pending"
-          :width="5"
+          :width="4"
         />
       </div>
-    </template>
-  </v-img>
+    </transition>
+  </div>
 </template>
 
 <script>
 import randomUtils from "@/utils/randomUtils";
 import CDN from "@/mixins/CDN";
+import service from "@/utils/service";
+import Console from "@/utils/Console";
 
 export default {
   name: "PreloaderInline",
@@ -36,7 +50,9 @@ export default {
     return {
       preloaders: [
         "croissant", "exusiai", "sora", "texas"
-      ]
+      ],
+      ready: true,
+      url: ''
     }
   },
   computed: {
@@ -44,25 +60,72 @@ export default {
       return randomUtils.randomInt(this.preloaders.length - 1)
     },
     currentSrc() {
-      return this.cdnDeliver(`/images/preloaders/${this.preloaders[this.currentPreloaderIndex]}.gif`)
+      return this.cdnDeliver(`/images/preloaders/${this.preloaders[this.currentPreloaderIndex]}.png`)
     },
     size () {
       if (this.small) {
         return {
-          image: 64,
+          image: {
+            "--size": "64px"
+          },
           pending: 32
         }
       } else {
         return {
-          image: 160,
+          image: {
+            "--size": "160px"
+          },
           pending: 48
         }
       }
+    }
+  },
+  created() {
+    this.update()
+  },
+  methods: {
+    update() {
+      this.ready = false
+      service.get(this.currentSrc, {
+        withCredentials: false,
+        responseType: "blob"
+      })
+        .then(({data}) => {
+          console.log(data)
+          const url = URL.createObjectURL(data)
+          console.log(url)
+          this.ready = true
+          this.url = url
+        })
+        .catch((err) => {
+          Console.info("PreloaderInline", "failed to update preloader image: ", err)
+          // we could do nothing here :(
+        })
     }
   }
 }
 </script>
 
 <style scoped>
-
+.animated-preloader {
+  height: var(--size);
+  width: var(--size);
+  animation: preloader infinite;
+  animation-duration: 270ms;
+  animation-timing-function: steps(3, end);
+  background-size: var(--size);
+  background-repeat: no-repeat;
+}
+@keyframes preloader {
+  from {
+    background-position: 0 0;
+  }
+  to {
+    background-position: 0 calc(-3 * var(--size));
+  }
+}
+.animated-preloader-wrapper {
+  height: var(--size);
+  width: var(--size);
+}
 </style>
