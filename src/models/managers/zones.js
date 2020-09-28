3f4f85236e3b8d400af4b7859e6c0ccec4eb8e60
GalvinGao <galvingaoper@gmail.com>
2020-09-29 07:09:54 +0800

    fix(managers): remove accidentally added test dataset

--- src/models/managers/zones.js
+++ src/models/managers/zones.js
@@ -17,34 +17,34 @@ const zones = new ObjectManager({
   },
   transform: [
     (object) => {
-      object.push({
-        "zoneId": "test01",
-        "zoneIndex": 0,
-        "type": "ACTIVITY",
-        "zoneName": "测试活动",
-        "zoneName_i18n": {"ja": "测试活动 ja", "ko": "测试活动 ko", "en": "测试活动 en", "zh": "测试活动 zh"},
-        "existence": {
-          "US": {"exist": true, "openTime": Date.now() + 10000, "closeTime": 1800000000000},
-          "JP": {"exist": false},
-          "CN": {"exist": true},
-          "KR": {"exist": false}
-        },
-        "stages": ["test01_001", "test01_002", "test01_003"]
-      })
-      object.push({
-        "zoneId": "test02",
-        "zoneIndex": 0,
-        "type": "ACTIVITY",
-        "zoneName": "测试活动",
-        "zoneName_i18n": {"ja": "测试活动 ja", "ko": "测试活动 ko", "en": "测试活动 en", "zh": "测试活动 2 全无dropInfos"},
-        "existence": {
-          "US": {"exist": true, "openTime": Date.now() + 10000, "closeTime": 1800000000000},
-          "JP": {"exist": false},
-          "CN": {"exist": true},
-          "KR": {"exist": false}
-        },
-        "stages": ["test02_001", "test02_002"]
-      })
+      // object.push({
+      //   "zoneId": "test01",
+      //   "zoneIndex": 0,
+      //   "type": "ACTIVITY",
+      //   "zoneName": "测试活动",
+      //   "zoneName_i18n": {"ja": "测试活动 ja", "ko": "测试活动 ko", "en": "测试活动 en", "zh": "测试活动 zh"},
+      //   "existence": {
+      //     "US": {"exist": true, "openTime": Date.now() + 10000, "closeTime": 1800000000000},
+      //     "JP": {"exist": false},
+      //     "CN": {"exist": true},
+      //     "KR": {"exist": false}
+      //   },
+      //   "stages": ["test01_001", "test01_002", "test01_003"]
+      // })
+      // object.push({
+      //   "zoneId": "test02",
+      //   "zoneIndex": 0,
+      //   "type": "ACTIVITY",
+      //   "zoneName": "测试活动",
+      //   "zoneName_i18n": {"ja": "测试活动 ja", "ko": "测试活动 ko", "en": "测试活动 en", "zh": "测试活动 2 全无dropInfos"},
+      //   "existence": {
+      //     "US": {"exist": true, "openTime": Date.now() + 10000, "closeTime": 1800000000000},
+      //     "JP": {"exist": false},
+      //     "CN": {"exist": true},
+      //     "KR": {"exist": false}
+      //   },
+      //   "stages": ["test02_001", "test02_002"]
+      // })
 
       object.map((el) => {
         el.icon = getIcon(el.type) || "mdi-help-circle";
