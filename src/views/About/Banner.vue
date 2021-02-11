e21e2e5adf318beab7b7061f51e9c20ff12ccc58
GalvinGao <galvingaoper@gmail.com>
2021-02-11 23:33:04 +0800

    improvement(theme): improved banner

--- src/views/About/Banner.vue
+++ src/views/About/Banner.vue
@@ -3,6 +3,8 @@
     color="primary"
     elevation="5"
     class="bkop-medium pa-6 py-12 d-flex align-center justify-center text-center position-relative"
+    style="box-shadow: inset 0 0 2rem black !important;"
+    dark
   >
     <v-img
       :src="require('@/assets/themes/new-year/banner.jpg')"
@@ -11,9 +13,14 @@
       style="position: absolute; top: 0; left: 0; opacity: 0.3; z-index: 0"
     />
 
-    <h1 class="display-1 position-relative">
-      企鹅物流数据统计恭祝各位刀客塔新年快乐，身体健康，万事如意
-    </h1>
+    <div class="position-relative">
+      <div class="headline mb-2">
+        企鹅物流数据统计恭祝各位刀客塔
+      </div>
+      <div class="display-1">
+        新年快乐，身体健康，万事如意
+      </div>
+    </div>
   </v-card>
 </template>
 
