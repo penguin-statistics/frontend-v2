9d2b63e2576a1dd82139886126571bccdacc0eb7
GalvinGao <me@galvingao.com>
2022-10-14 11:18:37 -0400

    style: lint

--- src/views/About/Changelog.vue
+++ src/views/About/Changelog.vue
@@ -1,5 +1,8 @@
 <template>
-  <v-timeline dense align-top>
+  <v-timeline
+    dense
+    align-top
+  >
     <v-timeline-item
       v-for="item in logs"
       :key="item.version"
@@ -16,9 +19,15 @@
       >
         <v-card-title class="py-2 pl-4 pr-2">
           <span class="title">
-            <span v-if="item.future" class="caption"> 正在开发 </span>
+            <span
+              v-if="item.future"
+              class="caption"
+            > 正在开发 </span>
 
-            <span v-if="item.active" class="caption"> 现正使用 </span>
+            <span
+              v-if="item.active"
+              class="caption"
+            > 现正使用 </span>
 
             {{ item.version }}
           </span>
@@ -33,9 +42,15 @@
             text
             @click.stop="item.value = !item.value"
           >
-            <span class="mr-2" v-text="formatTime(item.date)" />
+            <span
+              class="mr-2"
+              v-text="formatTime(item.date)"
+            />
 
-            <v-icon small v-text="item.value ? '$close' : 'mdi-calendar'" />
+            <v-icon
+              small
+              v-text="item.value ? '$close' : 'mdi-calendar'"
+            />
           </v-btn>
         </v-card-title>
 
@@ -43,7 +58,10 @@
           <div v-if="item.value">
             <v-divider />
 
-            <v-card-subtitle v-if="item.future" class="subtitle-1">
+            <v-card-subtitle
+              v-if="item.future"
+              class="subtitle-1"
+            >
               版本特性一览
             </v-card-subtitle>
 
@@ -67,11 +85,17 @@
               v-text="item.changes"
             />
 
-            <v-card-subtitle v-if="item.hotfix" class="subtitle-1">
+            <v-card-subtitle
+              v-if="item.hotfix"
+              class="subtitle-1"
+            >
               热修复记录
             </v-card-subtitle>
 
-            <v-card-text v-if="item.hotfix" class="markdown-content">
+            <v-card-text
+              v-if="item.hotfix"
+              class="markdown-content"
+            >
               <ol>
                 <li
                   v-for="(text, key) in item.hotfix"
