dcba288498fce41e56a8f937e35d2bc56289ddc6
GalvinGao <me@galvingao.com>
2023-02-19 22:39:23 -0500

    fix: kill service worker

--- src/main.js
+++ src/main.js
@@ -13,7 +13,6 @@ import environment from '@/utils/environment'
 
 import './injections'
 import PenguinProbe from '@/utils/probe'
-import './registerServiceWorker'
 
 if (!window.Intl) require('intl-collator')
 
