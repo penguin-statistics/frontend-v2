56af7e0d93066eae4a1b5a7574cf3aed653505c5
GalvinGao <me@galvingao.com>
2023-03-23 14:24:33 -0400

    feat: implement widget server sync; localized options

--- ios/App/App/Helpers.swift
+++ ios/App/App/Helpers.swift
@@ -129,33 +129,6 @@ public extension PenguinServer {
     }
 }
 
-public extension PenguinDynamicServer {
-    // synced property is PenguinDynamicServer(identifier: "synced", display: "PenguinDynamicServerTitleSyncWithApp"),
-    static func createSynced() -> PenguinDynamicServer {
-        return PenguinDynamicServer(identifier: "synced", display: "PenguinDynamicServerTitleSyncWithApp")
-    }
-    
-    static func createCN() -> PenguinDynamicServer {
-        return PenguinDynamicServer(identifier: "CN", display: "PenguinDynamicServerTitleCN")
-    }
-    
-    static func createUS() -> PenguinDynamicServer {
-        return PenguinDynamicServer(identifier: "US", display: "PenguinDynamicServerTitleUS")
-    }
-    
-    static func createJP() -> PenguinDynamicServer {
-        return PenguinDynamicServer(identifier: "JP", display: "PenguinDynamicServerTitleJP")
-    }
-    
-    static func createKR() -> PenguinDynamicServer {
-        return PenguinDynamicServer(identifier: "KR", display: "PenguinDynamicServerTitleKR")
-    }
-    
-    static func createDefault() -> PenguinDynamicServer {
-        return self.createSynced()
-    }
-}
-
 extension Optional where Wrapped: PenguinDynamicServer {
     var unwrapped: PenguinDynamicServer {
         switch self {
