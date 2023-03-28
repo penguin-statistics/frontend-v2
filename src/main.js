08d947461e7931f303317eb96a4b333f3340c5a3
GalvinGao <me@galvingao.com>
2023-03-28 17:22:31 -0400

    chore: remove dev

--- src/main.js
+++ src/main.js
@@ -1,12 +1,12 @@
-import FakeTimers from "@sinonjs/fake-timers"
-window.FakeTimers = FakeTimers.install({
-  now: new Date("2023-03-31T23:59:50.000+0800"),
-  shouldAdvanceTime: true,
-})
-
-setInterval(() => {
-  console.log("tick: ", new Date())
-}, 1000)
+// import FakeTimers from "@sinonjs/fake-timers"
+// window.FakeTimers = FakeTimers.install({
+//   now: new Date("2023-03-31T23:59:50.000+0800"),
+//   shouldAdvanceTime: true,
+// })
+//
+// setInterval(() => {
+//   console.log("tick: ", new Date())
+// }, 1000)
 
 import './workers/register'
 import 'core-js/stable'
