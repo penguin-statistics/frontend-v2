1752146e3347ca3dfdde990572df48627e08b67e
GalvinGao <me@galvingao.com>
2023-07-14 00:26:57 +0800

    build: add ci_scripts

---
+++ ios/App/ci_scripts/ci_post_clone.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+set -x
+
+export HOMEBREW_NO_INSTALL_CLEANUP=TRUE
+brew install cocoapods
+# have to add node yourself link to this issue https://stackoverflow.com/questions/73462672/xcode-cloud-suddenly-failing-to-link-node-and-install-dependencies
+NODE_VER=16
+VERSION=$(curl -s https://nodejs.org/dist/latest-v$NODE_VER.x/ | sed -nE 's|.*>node-(.*)\.pkg</a>.*|\1|p')
+if [[ "$(arch)" == "arm64" ]]; then
+  ARCH="arm64"
+else
+  ARCH="x64"
+fi
+
+curl "https://nodejs.org/dist/latest-v$NODE_VER.x/node-$VERSION-darwin-$ARCH.tar.gz" -o $HOME/Downloads/node.tar.gz
+tar -xf "$HOME/Downloads/node.tar.gz"
+NODE_PATH="$PWD/node-$VERSION-darwin-$ARCH/bin"
+PATH+=":$NODE_PATH"
+export PATH
+node -v
+npm -v
+
+npm install -g yarn
+
+# Install dependencies
+yarn install --frozen-lockfile
+
+# build and sync
+yarn run build:app
